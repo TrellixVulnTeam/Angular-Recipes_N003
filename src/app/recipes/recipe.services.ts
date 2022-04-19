ca933ae3f5bdd940b2f1d45f6ee81eb171ac2a01
morong34 <morong_pereg@yahoo.com>
2022-04-19 20:47:45 +0300

    Update de npm, added some redux stuff

--- src/app/recipes/recipe.services.ts
+++ src/app/recipes/recipe.services.ts
@@ -3,26 +3,17 @@ import {Injectable} from "@angular/core";
 import {Ingredient} from "../shared/ingredient.model";
 import {ShoppingListService} from "../shopping-list/shopping-list.service";
 import {Subject} from "rxjs";
+import {Store} from "@ngrx/store";
+import * as ShoppingListActions from "../shopping-list/store/shopping-list.actions";
 
 @Injectable()
 export class RecipeServices {
     recipeChanged = new Subject<Recipe[]>();
 
-    // private recipes: Recipe[] = [
-    //     new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
-    //         [
-    //             new Ingredient('Meat', 1),
-    //             new Ingredient('French Fries', 20)
-    //         ]),
-    //     new Recipe('A another Recipe', 'This is another a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
-    //         [
-    //             new Ingredient('Buns', 2),
-    //             new Ingredient('Bread',3)
-    //         ])
-    // ];
     private recipes: Recipe[] = [];
 
-    constructor(private slService: ShoppingListService) {}
+    constructor(private slService: ShoppingListService,
+                private store: Store<{ shoppingList: { ingredients: Ingredient[] } }>) {}
 
     setRecipes(recipes: Recipe[]) {
         this.recipes = recipes;
@@ -38,7 +29,8 @@ export class RecipeServices {
     }
 
     addIngredientsToShoppingList(ingredients: Ingredient[]) {
-        this.slService.addIngredients(ingredients);
+        // this.slService.addIngredients(ingredients);
+        this.store.dispatch(new ShoppingListActions.AddIngredients(ingredients))
     }
 
     addRecipe(recipe: Recipe) {
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -2,7 +2,8 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 
 import { Ingredient } from '../shared/ingredient.model';
 import {ShoppingListService} from "./shopping-list.service";
-import {Subscription} from "rxjs";
+import {Observable, Subscription} from "rxjs";
+import {Store} from "@ngrx/store";
 
 @Component({
   selector: 'app-shopping-list',
@@ -10,21 +11,23 @@ import {Subscription} from "rxjs";
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
-  ingredients: Ingredient[];
-  private igChangeSub: Subscription
+  ingredients: Observable<{ ingredients: Ingredient[] }>;
 
-  constructor(private slService: ShoppingListService) { }
+
+  constructor(private slService: ShoppingListService,
+              private store: Store<{shoppingList: {ingredients: Ingredient[] } }>) { }
 
   ngOnInit() {
-    this.ingredients = this.slService.getIngredients();
-    this.igChangeSub = this.slService.ingredientsChanged.subscribe((
-        ingredients: Ingredient[]) => {
-      this.ingredients = ingredients;
-    })
+    this.ingredients = this.store.select('shoppingList');
+    // this.ingredients = this.slService.getIngredients();
+    // this.igChangeSub = this.slService.ingredientsChanged.subscribe((
+    //     ingredients: Ingredient[]) => {
+    //   this.ingredients = ingredients;
+    // })
   }
 
   ngOnDestroy() {
-    this.igChangeSub.unsubscribe()
+    // this.igChangeSub.unsubscribe()
   }
 
   onEditItem(index: number) {
